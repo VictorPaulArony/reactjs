91b6426f8fbdc16d5094e8a48a93530409e7a307
victorpaularony <smallypauls@gmail.com>
2025-03-25 10:10:57 +0300

    UPDATE: the app to have form, nav sidebar and a form

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,11 +1,27 @@
 //import styling fot the react application 
 import './App.css';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import PostForm from './components/PostForm';
+import Card from './components/Card';
+
 
 //default function of the app page(landing page of the application)
 function App() {
   return (
     <div className = "App">
-      <h1>Hello Kenya 1 </h1>
+      <Navbar />
+      <div className='container'>
+        <Sidebar />
+        <main className='content'>
+          <PostForm />
+          <div className='card-container'>
+            <Card title="Post 1" description="This is the first post." />
+            <Card title="Post 2" description="This is the second post." />
+            <Card title="Post 3" description="This is the third post." />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }

7e34f4148f55bcda3ba99824e0d02acbf8bc1c49
victorpaularony <smallypauls@gmail.com>
2025-03-25 09:31:58 +0300

    FILE: App.js

---
+++ test-app/src/App.js
@@ -0,0 +1,13 @@
+//import styling fot the react application 
+import './App.css';
+
+//default function of the app page(landing page of the application)
+function App() {
+  return (
+    <div className = "App">
+      <h1>Hello Kenya 1 </h1>
+    </div>
+  );
+}
+
+export default App;

5acb2668f1975e7e6c7b86dac59c6d293426fd71
victorpaularony <smallypauls@gmail.com>
2025-03-29 00:20:09 +0300

    ADD: custom buttons that are predefined with different functionalities

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -5,11 +5,13 @@ import Sidebar from './components/Sidebar';
 import PostForm from './components/PostForm';
 import Card from './components/Card';
 import MessageButton from './components/buttons';
-import BasicButtons from './components/buttons';
+import { CustomButton, handleAlert, handleRedirect, useToggle } from './components/buttons';
 
 
 //default function of the app page(landing page of the application)
 function App() {
+  //use of the predifed toggle hook
+  const [isToggled, toggle] = useToggle()
   return (
     <div className="App">
       <Navbar />
@@ -24,8 +26,27 @@ function App() {
             <Card title="Post 2" description="This is the second post." />
             <Card title="Post 3" description="This is the third post." />
           </div>
-          <MessageButton />
         </main>
+      </div>
+
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Reusable Buttons with Different Functionalities</h1>
+
+        {/* Alert Button */}
+        <CustomButton label="Show Alert" onClick={handleAlert} />
+
+        {/* Redirect Button */}
+        <CustomButton label="Redirect to Home" onClick={handleRedirect} />
+
+        {/* Toggle Button */}
+        <CustomButton label="Toggle State" onClick={toggle} />
+        <p>Toggle State: {isToggled ? "ON" : "OFF"}</p>
+
+        {/* Custom Button with Inline Functionality */}
+        <CustomButton
+          label="Log to Console"
+          onClick={() => console.log("Button clicked!")}
+        />
       </div>
     </div>
   );

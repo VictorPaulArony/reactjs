779ac2914ea59668e8025d1bbc74d8b85c55f2c5
victorpaularony <smallypauls@gmail.com>
2025-04-18 02:06:55 +0300

    Function to show basic implimentation oc classess

--- components-classes-states/src/components/Classes.js
+++ components-classes-states/src/components/Classes.js
@@ -1,3 +1,15 @@
 import React from "react";
 
-//
+//react class component
+class SimpleClass extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Class component</h1>
+                <p>this is a simple class component</p>
+            </div>
+        )
+    }
+}
+
+export default SimpleClass;

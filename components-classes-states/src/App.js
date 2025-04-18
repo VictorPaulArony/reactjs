d3d9f675509786abd1590208070e3417cab81b25
victorpaularony <smallypauls@gmail.com>
2025-04-18 07:02:52 +0300

    added class component to the main App.js component

--- components-classes-states/src/App.js
+++ components-classes-states/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BasicComponent from './components/BasicComponent';
+import SimpleClass from './components/Classes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BasicComponent />
+      <SimpleClass />
     </div>
   );
 }

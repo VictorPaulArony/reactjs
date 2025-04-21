5b25e229a73b14bdb0db160e66eb95262eb25f8b
victorpaularony <smallypauls@gmail.com>
2025-04-21 17:40:50 +0300

    APP: move-shop app using react and tailwindcss for styling

---
+++ move-shop/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+        <h1 class="text-3xl font-normal text-center">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App

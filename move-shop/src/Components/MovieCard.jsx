5b57bdc9bd80f6117263087f37f2cec090df05ff
victorpaularony <smallypauls@gmail.com>
2025-04-22 21:54:19 +0300

    UPDATED:the move card to accept an overview of the movie with a length of 20

--- move-shop/src/Components/MovieCard.jsx
+++ move-shop/src/Components/MovieCard.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 
-const MovieCard = ({ movie: {title, poster_path, vote_average, original_language, release_date} }) => {
+
+
+const truncateText =(text, limit) => {
+  const words = text.split(' ');
+  return words.length > limit ? words.slice(0, limit).join(' ') + '...' : text;
+
+}
+
+const MovieCard = ({ movie: {title, overview, poster_path, vote_average, original_language, release_date} }) => {
+  const [showFullOverview, setShowFullOverview] = useState(false);
+
+const toggleOverview = () => {
+  setShowFullOverview(!showFullOverview);
+}
   return (
     <div className="movie-card bg-dark-100 text-amber-50 dark:bg-gray-800 rounded-2xl shadow-inner shadow-light-100/10 overflow-hidden transition-transform duration-300 hover:scale-105">
-      <img
-        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+      <img className='w-full h-auto'
+        src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : './src/assets/poster.png' }
         alt={title}
         />
         <h3 className='text-lg font-bold  m-2'>{title}</h3>
+        <p>
+          {showFullOverview ? overview : truncateText(overview, 20)}
+          {overview.length > 20 && (
+            <span className="text-blue-500 cursor-pointer" onClick={toggleOverview}>
+              {showFullOverview ? 'Show Less' : 'Show More'}
+            </span>
+          )}
+        </p>
       <div className="movie-overview flex gap-2 items-center align-middle">
         <div className='rating flex gap-2 items-center align-middle'>
             <img src="./src/assets/icon.png" alt="star icon" className='w-4 h-4 m-3' />

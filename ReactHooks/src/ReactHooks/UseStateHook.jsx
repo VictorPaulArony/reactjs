612f4b816740c5dfde68f9ffea86ac447e749409
victorpaularony <smallypauls@gmail.com>
2025-05-01 12:04:41 +0300

    UPDATE: color changes using React's useState hook, where each button click cycles through a list of predefined colors,

--- ReactHooks/src/ReactHooks/UseStateHook.jsx
+++ ReactHooks/src/ReactHooks/UseStateHook.jsx
@@ -8,11 +8,40 @@ const UseStateHook = () => {
             <h1>UseStateHook</h1>
             <p>counter: {counter}</p>
             <button onClick={() => setConter((counter) => counter + 1)}>Counter</button>
-           
+            <ColorPicker />
         </div>
     );
 };
 
 
+const ColorPicker = () => {
+    const colors = ['red', 'green', 'blue', 'black', 'orange', 'purple']
+    const [colorIndex, setColor] = useState(0)
+    
+    // function to change the color
+    function handleClick() {
+        // set the color to the next color in the array(cycle through colors)
+        setColor((prevIndex) => (prevIndex + 1)%colors.length)
+    }
+
+    return (
+        <div 
+            style={
+                { 
+                    height: '100px',
+                    margin: '20px',
+                    padding: '20px',
+                    backgroundColor: colors[colorIndex],
+                    borderRadius: '8px',
+                }
+            }
+        >
+            <p>The color is {colors[colorIndex]}</p>
+            <button onClick={handleClick}>Change Color</button>
+        </div>
+
+    )
+  
+  };
 
 export default UseStateHook;

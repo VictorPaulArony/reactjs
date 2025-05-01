58bdeecb8020d144bb841ee034685707baeab19e
victorpaularony <smallypauls@gmail.com>
2025-05-01 12:35:23 +0300

    UPDATE: //useEffect with try and catch block of async fuction

--- ReactHooks/src/ReactHooks/UseEffectHook.jsx
+++ ReactHooks/src/ReactHooks/UseEffectHook.jsx
@@ -23,11 +23,41 @@ const UseEffectHook = () => {
                     </li>
                 ))}
             </ol>
+            <UseEffectAsync />
         </div>
 
     );
 }
 
+//useEffect with try and catch block of async fuction
+const UseEffectAsync = () => {
+    const [users, setUsers] = useState([]);
 
+    const fetchUsers = async () => {
+        try {
+            const res = await fetch(API_URL)
+            const users = await res.json()
+            setUsers(users)
+
+        } catch (error) {
+            console.log("Failed to fetch users", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    return (
+        <div>
+            <h3>UseEffect with Async</h3>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}> {user.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 export default UseEffectHook

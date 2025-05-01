0f35e51c5bd865558005cacfcea2fd80f8104be7
victorpaularony <smallypauls@gmail.com>
2025-05-01 12:03:44 +0300

    UPDATE: the use of the useState hook - for local component Sate

--- ReactHooks/src/ReactHooks/UseStateHook.jsx
+++ ReactHooks/src/ReactHooks/UseStateHook.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
+//the use of the useState hook – For Local Component State
 const UseStateHook = () => {
+    const [counter, setConter] = useState(0);
     return (
         <div>
             <h1>UseStateHook</h1>
+            <p>counter: {counter}</p>
+            <button onClick={() => setConter((counter) => counter + 1)}>Counter</button>
+           
         </div>
     );
 };
+
+
 
 export default UseStateHook;

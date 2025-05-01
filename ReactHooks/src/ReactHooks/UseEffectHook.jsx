3086d081bb09668c460844977692597732eb13d7
victorpaularony <smallypauls@gmail.com>
2025-05-01 12:34:38 +0300

    UPDATE: useEffect – For Side Effects (e.g., API calls)

--- ReactHooks/src/ReactHooks/UseEffectHook.jsx
+++ ReactHooks/src/ReactHooks/UseEffectHook.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function UseEffectHook() {
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+//useEffect – For Side Effects (e.g., API calls)
+const UseEffectHook = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(users => setUsers(users))
+    }, [])
+
     return (
         <div>
             <h1>UseEffectHook</h1>
+            <h3>useEffect without async</h3>
+            <ol>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.name}
+                    </li>
+                ))}
+            </ol>
         </div>
+
     );
 }
+
+
+
+export default UseEffectHook

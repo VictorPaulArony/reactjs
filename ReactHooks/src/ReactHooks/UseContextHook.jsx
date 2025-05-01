5f40e00883c7a91f2472208bb8977a6ede3035d4
victorpaularony <smallypauls@gmail.com>
2025-05-01 11:19:17 +0300

    ADDED:  useContext – For Global State (with Context API)

---
+++ ReactHooks/src/ReactHooks/UseContextHook.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UseContextHook = () => {
+
+    return (
+        <div>
+            <h1>useContext</h1>
+        </div>
+    );
+}
+
+export default UseContextHook;
---
+++ ReactHooks/src/ReactHooks/UseEffectHook.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UseEffectHook() {
+    return (
+        <div>
+            <h1>UseEffectHook</h1>
+        </div>
+    );
+}

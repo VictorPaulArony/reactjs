d3c83c245129cf9cd8cac8a2bb6390745c887505
victorpaularony <smallypauls@gmail.com>
2025-05-27 03:40:09 +0300

    updated: function component to display weather forecast

--- WeatherApp/src/components/Days.jsx
+++ WeatherApp/src/components/Days.jsx
@@ -1,55 +1,32 @@
 import React from "react";
 
-const Days = () => {
-    const allDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// function component to display weather forecast
+const Days = ({ days }) => {
+  if (!days || days.length < 1) return null;
 
-    const getPastDays = () => {
-        const today = new Date().getDay()
-        const pastDays = []
+  const getDayName = (dateString) => {
+    const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const date = new Date(dateString);
+    return weekday[date.getDay()];
+  };
 
-        for (let i = 4; i > 0; i--) {
-            const dayId = (today - i + 7) % 7
-            pastDays.push(allDays[dayId])
-        }
-        return pastDays;
-    }
+  return (
+    <div className="forecast-section">
+      <div className="forecast-container">
+        {days.map((day, index) => (
+          <div key={index} className="card">
+            <div>{getDayName(day.dt_txt)}</div>
+            <img
+              src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+              alt="Weather Icon"
+            />
+            <div>{Math.round(day.main.temp)}°C</div>
+            <small>{day.weather[0].description}</small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    const setDays = getPastDays()
-    return (
-        <div
-            style={{
-                textAlign: 'center',
-                color: '#fff',
-            }
-            }>
-            <h2>last four days</h2>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                flexWrap: 'wrap',
-            }}>
-                {setDays.map((day, index) => (
-                    <div key={index} style={{
-                        border: '1px solid rgba(255, 255, 255, 0.5)',
-                        backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                        color: '#fff',
-                        padding: '5px',
-                        margin: '5px',
-                        height: '150px',
-                        width: '100px',
-                        textAlign: 'center',
-                        borderRadius: '8px',
-                        backdropFilter: 'blur(0.5px)',
-                    }}>{day}
-                        <img
-                            src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-                            alt="Weather Icon"
-                        />
-                    </div>
-                ))}
-
-            </div>
-        </div>
-    )
-}
 export default Days;
